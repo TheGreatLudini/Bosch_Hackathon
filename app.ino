d41d52d0917ace6729a2193986a1b7c2806cd6db
Ludwig <uietr@student.kit.edu>
2020-07-25 14:38:28 +0200

    Alles in eine Ino, Uebertragung von Skalarprodukt

--- app.ino
+++ app.ino
@@ -9,7 +9,7 @@
 
 #include "config.h"
 //#include "Motor.h"
-    
+
 // #define DEBUG_LOCAL_VECTOR
 // #define DEBUG_MISSALIGN
 // #define DEBUG_MOTOR
@@ -23,6 +23,8 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55);
 imu::Vector<3> wallNormal(0.0, 0.0, 0.0);
 imu::Vector<3> wall_X(0.0, 0.0, 0.0);
 imu::Vector<3> wall_Y(0.0, 0.0, 0.0);
+double localLeftRightError = 0.0;
+double localUpDownError = 0.0;
 
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
@@ -39,10 +41,14 @@ uint32_t interruptTime(0);
 uint8_t interruptCounter(0);
 
 //Motor myMotor = new Motor(MOTOR_FOR_DIR_PIN, MOTOR_BACK_DIR_PIN, MOTOR_SPEED_PIN);
-    
+
 void setup(void) 
 {
     Serial.begin(9600);
+    while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+    }
+
     Serial.println("Orientation Sensor Test"); Serial.println("");
     
     /* Initialise the sensor */
@@ -54,9 +60,10 @@ void setup(void)
     }
     
     delay(1000);
-    Serial.println("Setup almost done");
+
     bno.setExtCrystalUse(true);
-    Serial.println("Setup almost almost done");
+
+
     pinMode(INTERRUPT_PIN, INPUT);
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(LED_UP, OUTPUT);
@@ -68,10 +75,14 @@ void setup(void)
 
     attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), setAngle, FALLING);
     Serial.println("Setup done");
+
+    // HTTP server setup:
+    serverSetup();
 }
-    
+
 void loop(void) 
 {   
+    serverLoop();
     // The current orientation of the screw driver is stored as a quaternion in "quat":
     imu::Quaternion quat = bno.getQuat();
     imu::Vector<3> vectorToRotate(1.0, 0, 0);
@@ -97,8 +108,8 @@ void loop(void)
 
     // Angle error in up-down- and left-right-direction, determined via the dot product
     // If the dot product is 0, the respective axis is orthogonal to the wall normal, therefore good
-    double localLeftRightError = wallNormal.dot(yLocal);
-    double localUpDownError = wallNormal.dot(zLocal);
+    localLeftRightError = wallNormal.dot(yLocal);
+    localUpDownError = wallNormal.dot(zLocal);
 
     // LED on if the drilling angle is correct
     digitalWrite(LED_BUILTIN, abs(localLeftRightError) < ANGLE_DISPLACEMENT && abs(localUpDownError) < ANGLE_DISPLACEMENT);
@@ -274,4 +285,142 @@ void matrixMultip(double* matA, double* matB, double* matResult, int m, int p, i
     } else {
         Serial.println("Matrix dimensions do not match!");
     }
+}
+
+void serverSetup() {
+    Serial.println("Access Point Web Server");
+    // check for the WiFi module:
+    if (WiFi.status() == WL_NO_MODULE) {
+        Serial.println("Communication with WiFi module failed!");
+        // don't continue
+        while (true);
+    }
+
+    String fv = WiFi.firmwareVersion();
+    if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+        Serial.println("Please upgrade the firmware");
+    }
+
+    // by default the local IP address of will be 192.168.4.1
+    // you can override it with the following:
+    // WiFi.config(IPAddress(10, 0, 0, 1));
+
+    // print the network name (SSID);
+    Serial.print("Creating access point named: ");
+    Serial.println(ssid);
+
+    // Create open network. Change this line if you want to create an WEP network:
+    status = WiFi.beginAP(ssid);
+    if (status != WL_AP_LISTENING) {
+        Serial.println("Creating access point failed");
+        // don't continue
+        while (true);
+    }
+
+    // wait 10 seconds for connection:
+    delay(10000);
+
+    // start the web server on port 80
+    server.begin();
+
+    // you're connected now, so print out the status
+    printWiFiStatus();
+}
+
+void serverLoop() {
+    // compare the previous status to the current status
+    if (status != WiFi.status()) {
+        // it has changed update the variable
+        status = WiFi.status();
+
+        if (status == WL_AP_CONNECTED) {
+        // a device has connected to the AP
+            Serial.println("Device connected to AP");
+        } else {
+        // a device has disconnected from the AP, and we are back in listening mode
+            Serial.println("Device disconnected from AP");
+        }
+    }
+    
+    WiFiClient client = server.available();   // listen for incoming clients
+
+    if (client) {                             // if you get a client,
+        Serial.println("new client");           // print a message out the serial port
+        String currentLine = "";                // make a String to hold incoming data from the client
+        while (client.connected()) {            // loop while the client's connected
+            if (client.available()) {             // if there's bytes to read from the client,
+                char c = client.read();             // read a byte, then
+                Serial.write(c);                    // print it out the serial monitor
+                if (c == '\n') {                    // if the byte is a newline character
+
+                // if the current line is blank, you got two newline characters in a row.
+                // that's the end of the client HTTP request, so send a response:
+                if (currentLine.length() == 0) {/*
+                    // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                    // and a content-type so the client knows what's coming, then a blank line:
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-type:text/html");
+                    client.println();
+
+                    // the content of the HTTP response follows the header:
+                    client.print("Click <a href=\"/H\">here</a> turn the LED on<br>");
+                    client.print("Click <a href=\"/L\">here</a> turn the LED off<br>");
+
+                    // The HTTP response ends with another blank line:
+                    client.println();
+                    // break out of the while loop:
+                    */
+                    break;
+                } else {      // if you got a newline, then clear currentLine:
+                    currentLine = "";
+                }
+                } else if (c != '\r') {    // if you got anything else but a carriage return character,
+                    currentLine += c;      // add it to the end of the currentLine
+                }
+
+                // Check to see if the client request was "GET /H" or "GET /L":
+                if (currentLine.endsWith("GET /H")) {
+                    digitalWrite(LED_BUILTIN, HIGH);               // GET /H turns the LED on
+                }
+                if (currentLine.endsWith("GET /L")) {
+                    digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
+                }
+                if (currentLine.endsWith("GET /testValue")) {
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-type:text/plain");
+                    client.println();    
+
+                    client.print("geht es?????");
+                    client.println();
+                }
+                if (currentLine.endsWith("GET /getLocalTilt")) {
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-type:text/plain");
+                    client.println();    
+
+                    client.print(to_string(localLeftRightError) + "_" + to_string(localUpDownError));
+                    client.println();
+                }
+            }
+        }
+        // close the connection:
+        client.stop();
+        Serial.println("client disconnected");
+    }
+}
+
+void printWiFiStatus() {
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // print your WiFi shield's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+
+    // print where to go in a browser:
+    Serial.print("To see this page in action, open a browser to http://");
+    Serial.println(ip);
+
 }
