7534738e22f40772088ea1905055fc87c9293753
Ludwig <uietr@student.kit.edu>
2020-07-26 13:00:55 +0200

    Immer noch tausend bugs

--- AndroidApp/app/src/main/java/com/example/boostboxapp/GattAttributes.java
+++ AndroidApp/app/src/main/java/com/example/boostboxapp/GattAttributes.java
@@ -1,4 +1,23 @@
 package com.example.boostboxapp;
 
+import java.util.HashMap;
+
 public class GattAttributes {
+    private static HashMap<String, String> attributes = new HashMap();
+    public static String ARDUINO_SERVICE = "00002a37-0000-1000-8000-00805f9b34fb";
+    public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+
+    static {
+        // Sample Services.
+        attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
+        attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
+        // Sample Characteristics.
+        attributes.put(ARDUINO_SERVICE, "Arduino Service");
+        attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+    }
+
+    public static String lookup(String uuid, String defaultName) {
+        String name = attributes.get(uuid);
+        return name == null ? defaultName : name;
+    }
 }
